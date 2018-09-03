83ef1b6d9751d7b29284e9f241fc3ae1b13739fd
insideoutzombie <david@web3devs.com>
2018-09-03 00:25:55 -0500

    the code now takes an entire eth block header data and parse it into mongodb

--- block.js
+++ block.js
@@ -4,8 +4,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const BlockSchema = new Schema({
-  blockName: String,
-  resultData: String,
+  // blockName: String,
+  // resultData: String,
+  parentHash: String,
+  ommersHash: String,
+  beneficiary: String,
+  stateRoot: String,
+  transactionsRoot: String,
+  receiptsRoot: String,
+  logsBloom: String,
+  difficulty: String,
+  number: String,
+  gasLimit: String,
+  gasUsed: String,
+  timestamp: String,
+  extraData: String,
+  mixHash: String,
+  nonce: String,
 });
 
 // THIS associates the BlockSchema with mongoose
--- routes.js
+++ routes.js
@@ -4,9 +4,6 @@ var Block = require('./block');
 var request = require('superagent');
 var app = require('./app');
 var bodyParser = require('body-parser');
-// var app = express();
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
 
 module.exports = (app) => {
   // Watch for incoming requests of method GET and POST
@@ -31,12 +28,28 @@ let result;
             res.status(400).send("Unable to save to database :( sad face");
         });
 });
-//  =================================================================
+//  ==================================================================
 
 app.post("/api/block", (req, res) => {
+  console.log(result);
+  console.log(result.parentHash);
   let myData = new Block(
     {
-      resultData: result,
+      parentHash: result.parentHash,
+      ommersHash: result.ommersHash,
+      beneficiary: result.beneficiary,
+      stateRoot: result.stateRoot,
+      transactionsRoot: result.transactionsRoot,
+      receiptsRoot: result.receiptsRoot,
+      logsBloom: result.logsBloom,
+      difficulty: result.difficulty,
+      number: result.number,
+      gasLimit: result.gasLimit,
+      gasUsed: result.gasUsed,
+      timestamp: result.timestamp,
+      extraData: result.extraData,
+      mixHash: result.mixHash,
+      nonce: result.nonce,
     }
   );
   myData.save()
@@ -54,7 +67,7 @@ app.post("/api/block", (req, res) => {
 app.get("/api", (req, res) => {
   web3 =
     request
-    .get('https://api.infura.io/v1/jsonrpc/mainnet/eth_blockNumber')
+    .get('https://api.infura.io/v1/jsonrpc/mainnet/eth_getBlockByNumber?params=["latest",false]')
     .then(res => {
       result = res.body.result;
       console.log(result);
